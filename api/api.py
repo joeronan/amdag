a0728b7e8555ec5db30ae91bea7e7d0025642bda
joeronan <josephr409@hotmail.co.uk>
2021-05-08 14:19:33 +0100

    Implemented basic todo list

--- api/api.py
+++ api/api.py
@@ -1,9 +1,54 @@
-import time
-from flask import Flask
+from datetime import datetime
+from flask import Flask, request, redirect, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 
-@app.route('/time')
-def get_current_time():
-    return {'time': time.time()}
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f'<Task {self.id}>'
+
+
+@app.route('/todo', methods=['POST', 'GET', 'DELETE'])
+def index():
+    if request.method == 'POST':
+        task_dict = request.get_json()
+        task = Todo(content=task_dict['content'])
+
+        try:
+            db.session.add(task)
+            db.session.commit()
+            return 'Done', 201
+        except:
+            return 'There was an issue with your post task'
+
+    elif request.method == 'GET':
+        task_list = Todo.query.order_by(Todo.date_created).all()
+        tasks = []
+        for task in task_list:
+            tasks.append({
+                'id': task.id,
+                'content': task.content,
+                'date_created': task.date_created
+            })
+
+        return jsonify({'tasks': tasks})
+
+    elif request.method == 'DELETE':
+        task_dict = request.get_json()
+        task = Todo.query.get(task_dict['id'])
+
+        try:
+            db.session.delete(task)
+            db.session.commit()
+            return 'Done', 201
+        except:
+            return 'There was an issue with your delete task'
--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [todoList, setTodoList] = React.useState([])
+  const [newContent, setNewContent] = React.useState('')
+
+  const updateList = () => {
+    fetch('/todo', { method: 'GET' }).then(res => {
+      return (res.json())
+    }).then(data => {
+      setTodoList(data.tasks)
+    })
+  }
+
+  React.useEffect(updateList, [])
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +32,50 @@ function App() {
         >
           Learn React
         </a>
+
+        <form onSubmit={async e => {
+          e.preventDefault()
+          const entry = { content: newContent }
+          const response = await fetch('/todo', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(entry)
+          })
+
+          updateList()
+          setNewContent('')
+
+          if (response.ok) {
+            console.log('Response worked!')
+          }
+        }}>
+          <input type="text" value={newContent} onChange={e => setNewContent(e.target.value)} />
+          <input type="submit" value="Add Task" />
+        </form>
+        <ul>
+          {todoList.map(element =>
+            <li key={element.id}>
+              {element.date_created} {element.id}: {element.content}
+              <button onClick={
+                async e => {
+                  const entry = { id: element.id }
+                  const response = await fetch('/todo', {
+                    method: 'DELETE',
+                    headers: {
+                      'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(entry)
+                  })
+
+                  if (response.ok) {
+                    console.log('Deleted!')
+                    setTodoList(todoList.filter(x => x.id !== element.id))
+                  }
+                }}>X</button>
+            </li>)}
+        </ul>
       </header>
     </div>
   );
