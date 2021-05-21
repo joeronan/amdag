1858cd6b19f8f56c586cf3695139f55ba737a03d
joeronan <josephr409@hotmail.co.uk>
2021-05-21 15:14:55 +0100

    Created basic graph viewer

---
+++ src/graph.js
@@ -0,0 +1,86 @@
+import React from 'react'
+import { Zoom } from '@visx/zoom';
+
+
+function Graph({ width, height, graph, setSelectedElement }) {
+  const handleClick = (elementId) => {
+    setSelectedElement(elementId)
+  }
+
+  return (
+    <Zoom
+      width={width}
+      height={height}
+      scaleXMin={1 / 2}
+      scaleXMax={4}
+      scaleYMin={1 / 2}
+      scaleYMax={4}
+    >
+      {zoom => (
+        <div style={{ border: '10px solid black' }}>
+          <svg
+            width={width}
+            height={height}
+          >
+            <defs>
+              <marker id="dot" viewBox="0 0 10 10" refX="25" refY="5"
+                markerWidth="10" markerHeight="10" orient="auto">
+                <polygon points='0,0 0,10 5,5' fill='hotpink' />
+              </marker>
+              <pattern id="smallGrid" width="10" height="10" patternUnits="userSpaceOnUse">
+                <path d="M 10 0 L 0 0 0 10" fill="none" stroke="gray" stroke-width="0.5" />
+              </pattern>
+              <pattern id="grid" width="100" height="100" patternUnits="userSpaceOnUse">
+                <rect width="100" height="100" fill="url(#smallGrid)" />
+                <path d="M 100 0 L 0 0 0 100" fill="none" stroke="gray" stroke-width="1" />
+              </pattern>
+            </defs>
+
+            <g transform={zoom.toString()}>
+              <rect
+                x={-width * 5}
+                y={-height * 5}
+                width={width * 10}
+                height={height * 10}
+                fill="url(#grid)"
+              />
+            </g>
+
+            <rect
+              width={width}
+              height={height}
+              fill="transparent"
+              onTouchStart={zoom.dragStart}
+              onTouchMove={zoom.dragMove}
+              onTouchEnd={zoom.dragEnd}
+              onMouseDown={zoom.dragStart}
+              onMouseMove={zoom.dragMove}
+              onMouseUp={zoom.dragEnd}
+              onMouseLeave={() => {
+                if (zoom.isDragging) zoom.dragEnd();
+              }}
+              style={{ cursor: zoom.isDragging ? 'grabbing' : 'grab' }}
+            />
+            <g transform={zoom.toString()}>
+
+              {graph.map(element => {
+                return (<>
+                  <circle cx={element.x} cy={element.y} r='20' fill='hotpink' onClick={() => { handleClick(element.id) }} />
+                  <text x={element.x} y={element.y - 25} fill='hotpink'>{element.id}</text>
+                  {element.children.map(child => {
+                    const childElement = graph.filter((element) => element.id === child)[0]
+                    return (
+                      <line x1={element.x} y1={element.y} x2={childElement.x} y2={childElement.y} stroke='hotpink' marker-end='url(#dot)' />
+                    )
+                  })}
+                </>)
+              })}
+            </g>
+          </svg>
+        </div>
+      )}
+    </Zoom>
+  )
+}
+
+export default Graph
--- src/view-graph.js
+++ src/view-graph.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import SelectElementButton from './select-element-button'
+import Graph from './graph';
 
 function ViewGraph({ selectedElement, setSelectedElement }) {
+
+  const width = 800
+  const height = 800
 
   const [graph, setGraph] = React.useState([])
 
@@ -9,18 +13,27 @@ function ViewGraph({ selectedElement, setSelectedElement }) {
     fetch('/graph', { method: 'GET' }).then(res => {
       return (res.json())
     }).then(data => {
+      data.elements.forEach(element => {
+        element['x'] = Math.random() * width * 0.6 + width * 0.2
+        element['y'] = Math.random() * height * 0.6 + height * 0.2
+      });
       setGraph(data.elements)
     })
   }
 
   React.useEffect(updateGraph, [])
 
+
+
   return (
-    <ul>
-      {graph.map(element =>
-        <li key={element.id}>
-          {element.id}: {element.content} {element.parents}
-          <button onClick={
+    <>
+      {graph.filter((element) => element.id === selectedElement).map((element) => {
+        return <div>
+          <p>ID: {element.id} </p>
+          <p>Created: {element.date_created} </p>
+          <p>Last Edited: {element.date_edited} </p>
+          <p>{element.content}</p>
+          <p>Delete: <button onClick={
             async e => {
               const entry = { id: element.id }
               const response = await fetch('/element', {
@@ -30,15 +43,16 @@ function ViewGraph({ selectedElement, setSelectedElement }) {
                 },
                 body: JSON.stringify(entry)
               })
-
               if (response.ok) {
                 console.log('Deleted!')
                 setGraph(graph.filter(x => x.id !== element.id))
               }
-            }}>X</button>
-          <SelectElementButton element={element.id} selectedElement={selectedElement} setSelectedElement={setSelectedElement} />
-        </li>)}
-    </ul>
+            }}>X</button></p>
+        </div>
+      })}
+
+      <Graph width={width} height={height} graph={graph} setSelectedElement={setSelectedElement} />
+    </>
   )
 }
 
